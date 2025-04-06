bb95fb12828a46565528c9f6fc8c8cba4521069c
Jade-ok <jade.jinoklee@gmail.com>
2025-04-06 11:52:13 -0700

    final version

--- Project/server.js
+++ Project/server.js
@@ -1,35 +1,29 @@
 const express = require("express");
-const fs = require("fs");
-const cors = require("cors");
-const bodyParser = require("body-parser");
-const path = require("path");
-
-const app = express();
-const PORT = process.env.PORT || 3000; 
-
-app.use(cors());
-app.use(bodyParser.json());
-
-app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
-
-app.get("/api/comments", (req, res) => {
-  const data = fs.readFileSync("comments.json", "utf-8");
-  res.json(JSON.parse(data));
-});
-
-app.post("/api/comments", (req, res) => {
-  const newComment = req.body;
-  const data = fs.readFileSync("comments.json", "utf-8");
-  const comments = JSON.parse(data);
-  comments.push(newComment);
-  fs.writeFileSync("comments.json", JSON.stringify(comments, null, 2));
-  res.sendStatus(200);
-});
-
-app.listen(PORT, () => {
-  console.log(` Server is running on port ${PORT}`);
-});
+ const fs = require("fs");
+ const cors = require("cors");
+ const bodyParser = require("body-parser");
+ 
+ const app = express();
+ const PORT = 3000;
+ 
+ app.use(cors());
+ app.use(bodyParser.json());
+ app.use(express.static("public")); 
+ 
+ app.get("/api/comments", (req, res) => {
+   const data = fs.readFileSync("comments.json", "utf-8");
+   res.json(JSON.parse(data));
+ });
+ 
+ app.post("/api/comments", (req, res) => {
+    const newComment = req.body;
+    const data = fs.readFileSync("comments.json", "utf-8");
+    const comments = JSON.parse(data);
+    comments.push(newComment);
+    fs.writeFileSync("comments.json", JSON.stringify(comments, null, 2));
+    res.sendStatus(200);
+  });
+  
+  app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+  });
